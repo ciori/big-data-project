0768f6e424e4c950e4ee686a89d34c5932e4e7cc
ciori <mirkomarchiori94@gmail.com>
2018-12-01 18:12:32 +0100

    Added pre-processing and first counting

--- count_keywords.py
+++ count_keywords.py
@@ -6,6 +6,7 @@ from operator import add
 '''
  Iput: all pre-processed csv files of tweets ["user_id", "list of keywords in the tweet"]
  Output: single csv file containing semi-sorted, semi-reduced counts of keywords ["user_id_keyword", "count"]
+ ...
 '''
 
 # INITIALIZATION
