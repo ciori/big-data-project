f7fae11c09801d3200cebe62a57d4bbea6f87e67
ciori <mirkomarchiori94@gmail.com>
2019-01-03 17:32:59 +0100

    Update

--- user-profiling.py
+++ user-profiling.py
@@ -2,6 +2,9 @@ import csv
 import pandas as pd
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
+from pyspark.sql.types import LongType
+from pyspark.sql.types import StringType
 from operator import add
 
 # SPARK SESSION
@@ -15,7 +18,22 @@ ss = SparkSession \
 # SPARK CONTEXT
 sc = ss.sparkContext
 
-'''# flatmap function
+#schema = StructType().add("user_id", LongType(), False).add("keywords", StringType())
+
+# read tweet_keywords table
+tweet_keywords_DF = ss.read \
+    .format("jdbc") \
+    .option("url", "jdbc:mariadb://localhost:3306/mydb") \
+    .option("dbtable", "test") \
+    .option("user", "ciori") \
+    .option("password", "mariachepsw") \
+    .load()
+
+tweet_keywords_DF.printSchema()
+
+tweet_keywords_DF = tweet_keywords_DF.repartition("user_id")
+
+# flatmap function
 def init_counts(line):
     new_lines = []
     user_id = str(line[0])
@@ -23,26 +41,28 @@ def init_counts(line):
     for keyword in text.split(" "):
         #new_line = [user_id + "_" + keyword, 1]
         new_line = [user_id, keyword, 1]
+        #new_line = [keyword, 1]
         new_lines.append(new_line)
     return new_lines
 
-# read tweet_keywords table
-tweet_keywords_DF = ss.read \
-    .format("jdbc") \
-    .option("url", "jdbc:mariadb://localhost:3306/mydb") \
-    .option("dbtable", "tweet_keywords") \
-    .option("user", "ciori") \
-    .option("password", "mariachepsw") \
-    .load()
+# user profiling function
+def userprofiling(user_keywords):
+    #user_keywords_par = sc.parallelize(user_keywords)
+    user_keywords = sc.createDataFrame(user_keywords)
+    initial_keywords_counts = user_keywords.flatMap(lambda x: init_counts(x))
+    keywords_counts = initial_keywords_counts.reduceByKey(add)
 
-print(tweet_keywords_DF) '''
+tweet_keywords_DF.foreachPartition(userprofiling)
 
-df = ss.createDataFrame([[1, "ciao"], [2, "ciaociao"], [1, "come"], [2, "stai"]])
-df = df.repartition("_1")
+'''# TEST ----------------------
 
-def f(lines):
-    print(lines)
-    for line in lines:
-        print(line)
+tweet_keywords_DF = tweet_keywords_DF.repartition("user_id")
 
-df.foreachPartition(f)
+print("a")
+
+def userprofiling(user_keywords):
+    print("b")
+
+tweet_keywords_DF.foreachPartition(userprofiling)'''
+
+# TODO: passare a for che crea i dataframe -> userprofiling
