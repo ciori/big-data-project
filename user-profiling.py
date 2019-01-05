c2c7c0682aa544100ac34e01a7e11bd070158a14
ciori <mirkomarchiori94@gmail.com>
2019-01-05 17:56:21 +0100

    Update

--- user-profiling.py
+++ user-profiling.py
@@ -1,11 +1,14 @@
 import csv
-import pandas as pd
+import datetime
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType
-from pyspark.sql.types import LongType
-from pyspark.sql.types import StringType
 from operator import add
+
+print("Starting at: " + str(datetime.datetime.now()))
+
+# output users profiles csv
+output_csv = open("/home/ciori/Unitn/Big Data/tweets-database/user-profile/user_profile_top_10.csv", "a")
+output_reader = csv.writer(output_csv)
 
 # SPARK SESSION
 ss = SparkSession \
@@ -18,39 +21,60 @@ ss = SparkSession \
 # SPARK CONTEXT
 sc = ss.sparkContext
 
-# return list of user_id
-user_ids = ss.read \
-    .format("jdbc") \
-    .option("url", "jdbc:postgresql://localhost:5432/tweetsdb") \
-    .option("user", "postgres") \
-    .option("query", "select distinct user_id from public.test") \
-    .load()
+# split processing in batches of users
+for u in range(0, 8):
 
-# process tweets of each user
-for user_id in user_ids.collect():
+    print("Start processing next 10'000 users at: " + str(datetime.datetime.now()))
 
-    # read keywords
-    user_keywords = ss.read \
+    # return list of user_id batch
+    user_ids = ss.read \
         .format("jdbc") \
         .option("url", "jdbc:postgresql://localhost:5432/tweetsdb") \
         .option("user", "postgres") \
-        .option("query", "select * from public.test where user_id='" + str(user_id[0]) + "'") \
+        .option("query", "select distinct user_id from public.users_keywords order by user_id offset " + str(u) + "0000 limit 10000") \
         .load()
 
-    # flatmap function
+    min_id = user_ids.agg({"user_id": "min"}).collect()
+    max_id = user_ids.agg({"user_id": "max"}).collect()
+
+    print(min_id[0][0])
+    print(max_id[0][0])
+
+    # read keywords
+    users_keywords = ss.read \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost:5432/tweetsdb") \
+        .option("user", "postgres") \
+        .option("query", "select * from public.users_keywords where user_id>='" + str(min_id[0][0]) + "' and user_id<='" + str(max_id[0][0]) + "'") \
+        .load()
+
+    # init counts function
     def init_counts(line):
         new_lines = []
-        #user_id = str(line[0])
+        user_id = str(line[0])
         text = str(line[1])
         for keyword in text.split(" "):
-            #new_line = [user_id + "_" + keyword, 1]
+            new_line = [user_id + "_" + keyword, 1]
             #new_line = [user_id, keyword, 1]
-            new_line = [keyword, 1]
+            #new_line = [keyword, 1]
             new_lines.append(new_line)
         return new_lines
 
-    #user_keywords_par = sc.parallelize(user_keywords)
-    initial_keywords_counts = user_keywords.rdd.flatMap(lambda x: init_counts(x))
+    initial_keywords_counts = users_keywords.rdd.flatMap(lambda x: init_counts(x))
     keywords_counts = initial_keywords_counts.reduceByKey(add)
 
-    print(keywords_counts.collect())
+    # init lists function
+    def init_lists(line):
+        user_id = str(line[0].split("_")[0])
+        keywords = [[str(line[0].split("_")[1]), line[1]]]
+        return [user_id, keywords]
+
+    users_lists = keywords_counts.map(lambda x: init_lists(x))
+    users_profiles_raw = users_lists.reduceByKey(lambda x,y: x + y)
+
+    # save user profile into csv
+    output_reader.writerows(users_profiles_raw.collect())   
+
+    print("    done at: " + str(datetime.datetime.now()))
+
+print("Finished at: " + str(datetime.datetime.now()))
